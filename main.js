4f30c1db385cfce15d7845f02d21d167eb58ce86
DilmuhammadRustamov2014 <dilmuhammad.itf@gmail.com>
2025-02-06 22:50:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// const input = ("soz kiriting")
+
+// const h1 = document.createElement("h1")
+
+
+// document.body.appendChild("h1")
+
+//     form .addEventListener("submit", function(refreshBolma) {
+//     refreshBolma.preventDefault();
+//     }
+    
+
+// document.body.createElement.appendChild("h1")
+
+
+// const elform = document.getElementById("form");
+// const elinput = document.getElementById("user");
+
+
+// elform.addEventListener("submit", (anonim) => {
+//     anonim.preventDefault();
+//     const input.inputqiymati = elinput.value;
+
+//     const newI
+// })
+
